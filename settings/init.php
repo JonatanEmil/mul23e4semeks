f12114f9c602f3a32c094d3449d806b82b69de3b
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2025-04-23 11:24:07 +0200

    Database forberedelse

--- settings/init.php
+++ settings/init.php
@@ -35,10 +35,10 @@ $DB_PASS = "";
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0) {
-    $DB_SERVER = "mariadb";
-    $DB_NAME = "webshop";
-    $DB_USER = "user";
-    $DB_PASS = "password";
+    $DB_SERVER = "localhost";
+    $DB_NAME = "vinkompagni";
+    $DB_USER = "root";
+    $DB_PASS = "";
 } else if(CONFIG_LIVE == 1) {
     $DB_SERVER = "";
     $DB_NAME = "";
