2accb2feea518fd04807742568ad3a787949452d
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2025-04-25 14:02:54 +0200

    Progress på indsaetprodukt.php

--- indsaetprodukt.php
+++ indsaetprodukt.php
@@ -2,15 +2,17 @@
 /**
  * @var db $db
  */
-
 require "settings/init.php";
+$kategorier = $db->sql('SELECT * FROM kategorier ORDER BY katenavn ASC');
+$lande = $db->sql('SELECT * FROM lande ORDER BY landenavn ASC');
+$butikker = $db->sql('SELECT * FROM butikker ORDER BY butiknavn ASC');
 ?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
     <meta charset="utf-8">
     
-    <title>Sigende titel</title>
+    <title>Indsæt produkt</title>
     
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -23,7 +25,86 @@ require "settings/init.php";
 
 <body>
 
-<h1>Det virker 🥳</h1>
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-4">
+            Billeder her
+        </div>
+        <div class="col-8 row">
+            <div class="mb-3">
+                <label for="produktnavn" class="form-label">Indsæt produktnavn</label>
+                <input type="text" class="form-control" id="produktnavn">
+            </div>
+            <div class="col-4">
+                    <div class="dropdown mb-3">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="inside">
+                            Vælg kategori(er):
+                        </button>
+                        <ul class="dropdown-menu wide-dropdown">
+                            <?php foreach ($kategorier as $kategori) {?>
+                                <li><a class="dropdown-item" href = "#" ><?php echo $kategori->katenavn ?></a></li>
+                           <?php }?>
+                        </ul>
+                    </div>
+                <div class="dropdown mb-3">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="inside">
+                        Vælg kategori(er):
+                    </button>
+                    <ul class="dropdown-menu wide-dropdown">
+                        <?php foreach ($lande as $land) {?>
+                            <li><a class="dropdown-item" href = "#" ><?php echo $land->landenavn ?></a></li>
+                        <?php }?>
+                    </ul>
+                </div>
+                <div class="mb-3">
+                    <label for="omraade" class="form-label">Område:</label>
+                    <input type="text" class="form-control" id="omraade" placeholder="eksempel: Champagne">
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="dropdown mb-3">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="inside">
+                        Vælg kategori(er):
+                    </button>
+                    <ul class="dropdown-menu wide-dropdown">
+                        <?php foreach ($butikker as $butik) {?>
+                            <li><a class="dropdown-item" href = "#" ><?php echo $butik->butiknavn ?></a></li>
+                        <?php }?>
+                    </ul>
+                </div>
+                <div class="mb-3">
+                    <label for="pris" class="form-label">Pris</label>
+                    <input type="text" class="form-control" id="pris">
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        Kassepris
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+                            <label class="form-check-label" for="checkDefault">
+                            </label>
+                        </div>
+                    </div>
+                <div class="mb-3 col-10">
+                    <label for="kassepris" class="form-label"></label>
+                    <input type="text" class="form-control" id="kassepris" disabled>
+                </div>
+                </div>
+            </div>
+            <div class="col-4">
+
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
