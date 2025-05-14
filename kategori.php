22b484d39e1402963367ee49a24a35f9cdd5b2b1
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2025-05-14 08:21:00 +0200

    Igang med at få det hele fra databasen ind korrekt. Smagekasser opfører sig underligt

--- kategori.php
+++ kategori.php
@@ -8,10 +8,9 @@ require "settings/init.php";
 $bind = [];
 
 if (!empty($_GET['kateid'])) {
-    $sqladd = "kateid = :kateid";
     $bind[":kateid"] = $_GET['kateid'];
 }
-$produkter = $db->sql('SELECT * FROM produkter INNER JOIN `prod-kat-con` ON prodid = prkacoprodid WHERE prkacokatid = :kateid', $bind);
+$produkter = $db->sql('SELECT * FROM produkter INNER JOIN lande ON prodland = landeid INNER JOIN `prod-kat-con` ON prodid = prkacoprodid WHERE prkacokatid = :kateid', $bind);
 
 ?>
 <!DOCTYPE html>
@@ -30,17 +29,48 @@ $produkter = $db->sql('SELECT * FROM produkter INNER JOIN `prod-kat-con` ON prod
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-champagne">
 <?php include "header.php"; ?>
 <div class="container">
-    <div class="row">
-        <?php
-        foreach ($produkter as $produkt) { ?>
-            <div class="col-2"><a><img class="img-fluid" src="img/produkter/<?php echo $produkt->prodimg ?>"
-                                       alt="<?php echo $produkt->prodnavn ?> - Vinkompagnierne"></a></div>
-        <?php } ?>
-    </div>
+    <div class="row g-4 d-flex page-content ">
+        <div class="col-3">
+            Filter menu
+        </div>
+        <div class="col-9">
+            <div class="row g-3">
+                <?php foreach ($produkter as $produkt) { ?>
+                    <div class="col-md-4 d-flex">
+                        <div class="card mb-3 p-2 h-100 w-100">
+                            <!-- card content here -->
+                            <h5 class="card-title"><?php echo $produkt->prodnavn ?></h5>
+                            <div class="row">
+                                <div class="col-4 d-flex justify-content-center">
+                                    <img src="img/produkter/<?php echo $produkt->prodimg ?>" class="img-fluid" alt="<?php echo $produkt->prodnavn ?> - Vinkompagnierne" style="height: 20vh">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                    <p>Kr. <?php echo $produkt->prodpris ?> pr. stk.</p>
 
+                                        <?php if ($produkt->prodkasse === 1) {
+                                            echo "<p>Kr. ".  $produkt->prodkassepris . " pr. kasse</p>";
+                                        }
+                                        if (!empty($produkt->prodland)) {
+                                       echo '<p class="card-text">' . $produkt->landenavn .'</p>';
+                                        }
+                                        if (!empty($produkt->prodomraade)) {
+                                       echo '<p class="card-text">' . $produkt->prodomraade .'</p>';
+                                          }
+                                        ?>
+                                        <a class="stretched-link" href="produkt.php?kateid=<?php echo $produkt->prkacokatid ?>&prodid=<?php echo $produkt->prodid; ?>"></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
--- produkt.php
+++ produkt.php
@@ -4,13 +4,22 @@
  */
 
 require "settings/init.php";
+$bind = [];
+
+if (!empty($_GET['kateid']) && !empty($_GET['prodid'])) {
+    $bind[":kateid"] = $_GET['kateid'];
+    $bind[":prodid"] = $_GET['prodid'];
+}
+$produkter = $db->sql('SELECT * FROM produkter INNER JOIN lande ON prodland = landeid INNER JOIN `prod-kat-con` ON prodid = prkacoprodid WHERE prkacokatid = :kateid AND prodid = :prodid', $bind);
+$produkt = $produkter[0];
+$madikoner = $db->sql('SELECT * FROM `prod-mad-con` INNER JOIN madikoner ON prmacoikonid = ikonid WHERE prmacoprodid = :prodid', [":prodid" => $produkt->prodid]);
 ?>
 <!DOCTYPE html>
 <html lang="da">
 <head>
     <meta charset="utf-8">
 
-    <title>Sigende titel</title>
+    <title><?php echo $produkt->prodnavn; ?> hos Vinkompagnierne</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -21,36 +30,31 @@ require "settings/init.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body class="bg-champagne">
 <?php include "header.php"; ?>
 <div class="container">
-    <div class="row g-3">
-        <?php
-        $produkter = $db->sql("SELECT * FROM produkter");
-        foreach($produkter as $produkt) {
-            ?>
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <?php
-                        echo $produkt->prodNavn;
-                        ?>
-                    </div>
-                    <div class="card-body">
-                        <?php
-                        // Indsæt andet felt fra database
-                        ?>
-                    </div>
-                    <div class="card-footer">
-                        <?php
-                        // Indsæt andet felt fra database
-                        ?>
-                    </div>
-                </div>
+    <div class="row g-3 page-content">
+        <div class="col-5">
+            <div class="d-flex justify-content-center">
+                <img class="" src="img/produkter/<?php echo $produkt->prodimg; ?>" alt="<?php echo $produkt->prodnavn; ?> - Vinkompagnierne">
             </div>
-            <?php
-        }
-        ?>
+        </div>
+        <div class="col-7">
+            <h1 class="mb-0"><?php echo $produkt->prodnavn;?></h1>
+            <p><?php echo $produkt->landenavn;?></p>
+            <?php foreach ($madikoner as $ikon) {?>
+                <img class="img-fluid" style="height: 3vw" src="img/ikoner/<?php echo $ikon->ikonimg?>" alt="<?php echo $ikon->ikonnavn?>">
+            <?php } ?>
+            <p class="display-2 fw-bold">Kr. <?php echo $produkt->prodpris?> pr. stk.</p>
+            <?php if ($produkt->prodkasse === 1) {?>
+                <p class="h5 fw-bold">Kassepris (6 flasker): Kr. <?php echo $produkt->prodkassepris?> pr. kasse.</p>
+            <?php }?>
+            <select class="form-select" aria-label="Default select example"> <!-- Se kim video vil gerne have den expanded -->
+                <option value="1"></option>
+                <option value="2"></option>
+                <option value="3"></option>
+            </select>
+        </div>
     </div>
 </div>
 
