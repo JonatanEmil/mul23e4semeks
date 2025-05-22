b7a513231e4771ea29382f165c4063b9157c26bc
Kitsune <sheenatheinari@gmail.com>
2025-05-22 12:35:54 +0200

    Har fået lavet lidt på footer, dette er kun for mobil/tablet størrelse indtil videre. Der skal finjusteres og der mangler helt sikkert stadig nogle ting til det osv.

--- footer.php
+++ footer.php
@@ -1,1 +1,88 @@
 <?php
+?>
+
+<footer class="footer-downer bg-wine bottom-100 text-champagne">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mt-4">
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">Vinkompagni</p>
+                <p class="mb-1">Middelfart 2, 4200 Slagelse</p>
+                <p class="mb-1">Danmark</p>
+                <p class="mb-1">+45 58 56 14 00</p>
+                <p class="mb-1">Email: ordre@slagelsevinkompagni.dk</p>
+                <p class="mb-1">Cvr. nummer: 2723 4720</p>
+            </div>
+            <div class="col-12 mt-4">
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">Information</p>
+                <p class="mb-1">Om os</p>
+                <p class="mb-1">Bestilling og levering</p>
+                <p class="mb-1">Kontakt os</p>
+                <p class="mb-1">Hjælp</p>
+            </div>
+            <div class="col-12 mt-4">
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">Åbningstider</p>
+                <div class="row">
+                    <div class="col-3">
+                        Mandag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-17</p>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3">
+                        Tirsdag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-17</p>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3">
+                        Onsdag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-17</p>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3">
+                        Torsdag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-17</p>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3">
+                        Fredag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-17</p>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3">
+                        Lørdag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-14</p>
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-3">
+                        Søndag
+                    </div>
+                    <div class="col-5 border-bottom"></div>
+                    <div class="col-3">
+                        <p class="m-0">10-14</p>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-4">
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">SoMe & Sponsorer</p>
+            </div>
+        </div>
+    </div>
+</footer>
