ad125fe7b6e76a0d47cd5390675a46f1d035545b
Kitsune <sheenatheinari@gmail.com>
2025-05-23 12:13:43 +0200

    Har siddet og leget med farver, da ikonerne i mobil størrelse mangler kontrast, så deres detaljer ikke forsvinder, har derfor prøvet med gradient så der er kontrast fra baggrunden og ikonnerne. Har også addet ikoner til SoMe fra 'Font Awesome' og også copyright symbol.

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
 <?php
 ?>
 
-<footer class="footer-downer bg-wine bottom-100 text-champagne">
+<script src="https://kit.fontawesome.com/96a3a7d865.js" crossorigin="anonymous"></script>
+
+<footer class="footer-downer bg-wine bottom-100 text-champagne mt-5 pb-5 border-champagne" style="background-image: linear-gradient(to top, #333333, #642A38);">
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-3 mt-4">
-                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">Vinkompagni</p>
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 pb-3">Vinkompagni</p>
                 <p class="mb-1">Middelfart 2, 4200 Slagelse</p>
                 <p class="mb-1">Danmark</p>
                 <p class="mb-1">+45 58 56 14 00</p>
@@ -13,14 +15,14 @@
                 <p class="mb-1">Cvr. nummer: 2723 4720</p>
             </div>
             <div class="col-12 col-lg-3 mt-4">
-                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">Information</p>
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 pb-3">Information</p>
                 <p class="mb-1">Om os</p>
                 <p class="mb-1">Bestilling og levering</p>
                 <p class="mb-1">Kontakt os</p>
                 <p class="mb-1">Hjælp</p>
             </div>
             <div class="col-12 col-lg-3 mt-4">
-                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">Åbningstider</p>
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 pb-3">Åbningstider</p>
                 <div class="row">
                     <div class="col-3">
                         Mandag
@@ -81,7 +83,20 @@
                 </div>
             </div>
             <div class="col-12 col-lg-3 mt-4">
-                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 border-champagne pb-3">SoMe & Sponsorer</p>
+                <p class="h2 mb-4 fw-bolder text-center border-bottom border-3 pb-3 fs-2">SoMe & Sponsorer</p>
+                <div class="d-flex row-cols-4 text-center">
+                    <a><i class="fa-brands fa-facebook"></i></a>
+                    <a><i class="fa-brands fa-instagram "></i></a>
+                    <a><i class="fa-brands fa-linkedin"></i></a>
+                    <a><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+            <div class="col-12 mt-4">
+                <p class="h2 mb-4 text-center">Betalings muligheder</p>
+                </div>
+            </div>
+            <div class="col-12 text-center mt-5 mb-5">
+                <p><i class="fa-regular fa-copyright"></i> Copyright 2025</p>
             </div>
         </div>
     </div>
