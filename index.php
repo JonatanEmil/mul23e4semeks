8029d70fe362a5189536be4d099d9334a8bca79c
Jonatan Emil Rytter <jor001@edu.zealand.dk>
2025-05-23 14:52:21 +0200

    Slider der næsten virker! Kan kun virke som prototype.

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ require "settings/init.php";
     <meta name="copyright" content="Information om copyright">
     
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -27,16 +26,11 @@ require "settings/init.php";
 <div class="container page-content bg-cream">
     <div class="row pt-3">
         <div class="col-12">
-            <h1>Det virker 🥳</h1>
+
         </div>
     </div>
 </div>
 
-<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DJBFWu_AkD8/" data-instgrm-version="14"></blockquote>
-<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DA6h3AzCY53/?img_index=2" data-instgrm-version="14"></blockquote>
-<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DIL71M3Kemz" data-instgrm-version="14"></blockquote>
-<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/CikuD-Hr0oO/" data-instgrm-version="14"></blockquote>
-<blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/C_I7AEuvOh7/" data-instgrm-version="14"></blockquote>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> <!-- Denne lå i index
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script> denne lå i products, hvilken skal vi bruge?????+-->
@@ -48,6 +42,15 @@ require "settings/init.php";
     dropupElement.addEventListener('shown.bs.dropdown', function () {
         input.focus();
     });
+    const cards = document.querySelectorAll('.flip-card');
+    let flipState = false;
+
+    setInterval(() => {
+        cards.forEach(card => {
+            card.classList.toggle('auto-flip', flipState);
+        });
+        flipState = !flipState;
+    }, 4000); // every 4 seconds
 </script>
 </body>
 </html>
